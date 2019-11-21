a4a1b86f0d12f171234242b7d206f55c315998b4
Hegar Garcia <hegargarcia@gmail.com>
2019-11-20 20:01:54 -0600

    Implementing dynamic tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { IonApp } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-
+import { IonApp } from "@ionic/react";
 import "./styles";
 
 import Router from "./components/Router";
-import Tabs from "./components/Tabs";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <Router />
-      <Tabs />
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <Router />
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Route, Redirect } from "react-router";
+import React, { FC } from "react";
+import { Route, Redirect, withRouter, RouteComponentProps } from "react-router";
 
 import * as Routes from "../constants/routes";
 
+import Tabs from "../components/Tabs";
 import Clients from "../pages/Clients";
 import Products from "../pages/Products";
 import Sells from "../pages/Sells";
@@ -10,18 +11,26 @@ import Stats from "../pages/Stats";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 
-const Router = () => (
-  <>
-    <Route path={Routes.CLIENT} component={Clients} exact={true} />
-    <Route path={Routes.PRODUCT} component={Products} exact={true} />
-    <Route path={Routes.SELL} component={Sells} exect={true} />
-    <Route path={Routes.STATS} component={Stats} exect={true} />
+const Router: FC<RouteComponentProps> = props => {
+  const currentRoute = props.location.pathname;
+  const isAuthRoute =
+    currentRoute === Routes.SIGNIN || currentRoute === Routes.SIGNUP;
 
-    <Route path={Routes.SIGNUP} component={SignUp} exect={true} />
-    <Route path={Routes.SIGNIN} component={SignIn} exect={true} />
+  return (
+    <>
+      <Route path={Routes.CLIENT} component={Clients} exact={true} />
+      <Route path={Routes.PRODUCT} component={Products} exact={true} />
+      <Route path={Routes.SELL} component={Sells} exect={true} />
+      <Route path={Routes.STATS} component={Stats} exect={true} />
 
-    <Redirect path={Routes.ROOT} to={Routes.CLIENT} exact={true} />
-  </>
-);
+      <Route path={Routes.SIGNUP} component={SignUp} exect={true} />
+      <Route path={Routes.SIGNIN} component={SignIn} exect={true} />
 
-export default Router;
+      <Redirect path={Routes.ROOT} to={Routes.CLIENT} exact={true} />
+
+      {!isAuthRoute && <Tabs />}
+    </>
+  );
+};
+
+export default withRouter(Router);
