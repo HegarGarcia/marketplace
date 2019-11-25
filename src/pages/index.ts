9afb2aef9007b27a982cb37022b826b416810413
Hegar Garcia <hegargarcia@gmail.com>
2019-11-25 09:44:49 -0600

    Adding index file as entry file for pages folder

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as ClientList } from "./ClientsList";
+export { default as ProductList } from "./ProductsList";
+export { default as SellList } from "./SellList";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
+export { default as Stats } from "./Stats";
