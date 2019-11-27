a6a01e82067315e79fc59fd04cfeefe31c4142b5
Hegar Garcia <hegargarcia@gmail.com>
2019-11-26 20:57:21 -0600

    Adding routes for detail page

--- src/pages/sells/Sell.tsx
+++ src/pages/sells/Sell.tsx
@@ -3,11 +3,14 @@ import { IonRouterOutlet } from "@ionic/react";
 import { Route } from "react-router";
 import { SELL } from "../../constants/routes";
 import SellList from "./SellList";
+import SellDetail from "./SellDetail";
 
-const Product = () => (
+const Sell = () => (
   <IonRouterOutlet>
-    <Route path={SELL} component={SellList} />
+    <Route path={SELL} component={SellList} exact={true} />
+    <Route path={`${SELL}/detail`} component={SellDetail} exact={true} />
+    <Route path={`${SELL}/detail/:id`} component={SellDetail} exact={true} />
   </IonRouterOutlet>
 );
 
-export default Product;
+export default Sell;
