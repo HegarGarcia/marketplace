627f96090ec8e3afb54ecff4ecf8f8cc6759237b
Hegar Garcia <hegargarcia@gmail.com>
2019-11-26 20:57:29 -0600

    Sells List page

--- src/pages/sells/SellList.tsx
+++ src/pages/sells/SellList.tsx
@@ -1,24 +1,72 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
 import {
-  IonHeader,
-  IonToolbar,
+  IonContent,
   IonPage,
-  IonTitle,
-  IonContent
+  IonFab,
+  IonFabButton,
+  IonIcon
 } from "@ionic/react";
+import { add } from "ionicons/icons";
+import List, { Item } from "../../components/List";
+import { sellsCollection, Product, productsCollection } from "../../firebase";
+import { Sell } from "../../firebase";
+import Header from "../../components/Header";
+import { SELL } from "../../constants/routes";
 
-const Sells: React.FC = () => {
+const SellsList: FC = () => {
+  const [sells, setSells] = useState<Item[]>([]);
+
+  useEffect(() => {
+    sellsCollection.onSnapshot(async ({ docs }) => {
+      const items = await Promise.all(
+        docs.map(async doc => {
+          const sell = {
+            id: doc.id,
+            ...(doc.data() as Sell)
+          };
+
+          const product = await productsCollection
+            .doc(sell.product)
+            .get()
+            .then(productDoc => productDoc.data() as Product);
+
+          return sellToItem({ ...sell, product });
+        })
+      );
+
+      setSells(items);
+    });
+  }, []);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Sell</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header title='Sell' withBackButton={false} />
 
-      <IonContent />
+      <IonContent>
+        <List items={sells} rootPath={SELL} />
+
+        <IonFab vertical='bottom' horizontal='end'>
+          <IonFabButton routerLink={`${SELL}/detail`}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
     </IonPage>
   );
 };
 
-export default Sells;
+interface SellWithProduct {
+  id?: string;
+  product: Product;
+  amount: number;
+  total: number;
+}
+
+const sellToItem = ({ id, product, total }: SellWithProduct): Item => ({
+  id: id!,
+  title: (product as Product).description,
+  subtitle: `$${total}`,
+  photoUrl: (product as Product).photoUrl
+});
+
+export default SellsList;
